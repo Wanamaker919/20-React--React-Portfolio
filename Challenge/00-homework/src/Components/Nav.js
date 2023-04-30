fc484aa9490c5f53ea1333fb281cce942ead3761
Wanamaker919 <116662904+Wanamaker919@users.noreply.github.com>
2023-04-30 14:32:54 -0400

    alot of new changes Nav about js the React is up and working

---
+++ Challenge/00-homework/src/Components/Nav.js
@@ -0,0 +1,15 @@
+import '../App.css'
+
+function Navapp(){
+    return (
+        <div> 
+          <nav>
+        <a href="#">About Me</a>
+        <a href="#">Work</a>
+        <a href="#">Contact Me</a>
+        <a href="#">Resume</a>
+        </nav> 
+
+        </div>
+    )
+}
---
+++ Challenge/00-homework/src/Components/about.js
@@ -0,0 +1,54 @@
+import ''
+
+
+function portfoiloApp() {
+    return (
+<div>
+        <body>
+    <header>
+        <h1> LaShawn Wanamaker</h1>
+        <img src="https://via.placeholder.com/150">
+        
+    </header>
+    <nav>
+        <a href="#">About Me</a>
+        <a href="#">Work</a>
+        <a href="#">Contact Me</a>
+        <a href="#">Resume</a>
+      </nav> 
+    <section>
+        <h2>About Me</h2>
+        <p>My name is LaShawn Wanamaker. I am a UNC Bootcamp student and I am loving the class and all 
+            the new information and ideas that I am learning, plus its really fun. Give me a JOB!!!
+        </p>
+    </section>
+
+    <section>
+        
+            <h3>Work</h3>
+          </div>
+          <div class="example">
+            <img alt="Placeholder" src="https://github.com/Catalystix/SuperGroupOne.git">
+            <h3>Contact Me</h3>
+          </div>
+          <div class="example">
+            <img alt="Placeholder" src="https://via.placeholder.com/150">
+            <h3>Resume</h3>
+          </div>
+        </section>
+        <section>      
+            <h2>Contact Info</h2>
+            <form action="">
+                <p>
+                    <label  for="email">E-mail:</label>
+                    <input id="email" type="text" placeholder="me@example.com" />
+                </p>
+            </form>
+        </section>
+        </body>
+
+    </div>
+);
+}
+
+export default portfoiloApp;
