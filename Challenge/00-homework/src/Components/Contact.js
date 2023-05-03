76ec234f871abf359c8c884373c8573e299e533f
Wanamaker919 <116662904+Wanamaker919@users.noreply.github.com>
2023-05-03 18:30:31 -0400

    new changes

--- Challenge/00-homework/src/Components/Contact.js
+++
@@ -1,6 +0,0 @@
-function ContactApp(){
-    return(
-    
-    )
-}
-export default ContactApp;
---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import FooterApp from "./Footer";
+function HeaderApp() {
+    return (
+        <div>
+
+            <header>
+                <img src="https://via.placeholder.com/150"></img>
+                <h2>About Me</h2>
+                <p>My name is LaShawn Wanamaker. I am a UNC Bootcamp student and I am loving the class and all
+                    the new information and ideas that I am learning, plus its really fun. Give me a JOB!!!
+                </p>
+
+
+            </header>
+            <FooterApp></FooterApp>
+        </div>
+    )
+}
+export default HeaderApp;
